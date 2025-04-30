1dfa2a42874c8c5a283127c00a1c62fe7398a129
Berk <banbar@Berk-MacBook-Air.local>
2025-04-30 11:47:00 +0300

    update code

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -5,7 +5,7 @@ def bubble_sort( theSeq ):
         n = len( theSeq )
         for i in range( n - 1 ):
             for j in range( n-(i+1) ):
-                if (theSeq[j] > theSeq[j]):
+                if (theSeq[j] > theSeq[j + 1]):
                     tmp = theSeq[j]
                     theSeq[j] = theSeq[j + 1]
                     theSeq[j + 1] = tmp
